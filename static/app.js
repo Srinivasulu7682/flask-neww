35c53512295a82790484b0817aab2a659fdeacfc
Srinivasulu768 <147645181+Srinivasulu768@users.noreply.github.com>
2023-10-28 20:26:12 +0530

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,4 @@
+// Add your scripts here
+document.addEventListener('DOMContentLoaded', () => {
+  // Add your JavaScript functionality here
+});
---
+++ static/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/static/css/styles.css', // Update with your CSS file path
+                '/static/js/app.js'       // Update with your JS file path
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
